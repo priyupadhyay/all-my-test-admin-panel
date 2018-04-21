b0be12d63561c828ae62aa282f3c3de94bb1cf43
imran-elezire <32159477+imran-elezire@users.noreply.github.com>
2018-04-21 22:12:37 +0530

    1

---
+++ firewall/index.php
@@ -0,0 +1,9 @@
+<?php
+ include '../parts/header_one.php';
+ include '../parts/header_two.php';
+ include '../parts/nav.php';
+ include '../parts/navtop.php';
+ include '../parts/footer_one.php';
+ include '../parts/footer_two.php';
+
+?>
